72c5b89042638f0342f1531f7aea8095222102bd
Rashed <Rashed@DESKTOP-KOGV4EJ>
2025-03-26 04:20:06 +0200

    Add project files.

---
+++ Session2/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Session2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult GetMovie(int id, string Name)
+        {
+            //return $"Movie With Id: {id}";
+
+            //ContentResult result = new ContentResult();
+            //result.Content = $"Movie With Id: {id}";
+            //return result;
+            return Content($"Movie With Id: {id}"); //Helper Method Inherted From ControllerBased
+
+        }
+        //[ActionName("Index")]
+        //[HttpGet]
+        public IActionResult TestRedirectToAction()
+        {
+            //RedirectResult result = new RedirectResult("URL");
+            //return result;
+            //return Redirect("URL");
+            return RedirectToAction("GetMovie");
+        }
+        //public IActionResult TestModelBinding(int id , string tittle ,Movie movie) 
+        //{
+        //    return View(Movie);
+        //}
+
+        public IActionResult TestModelBindingFromRequest([FromBody] Movie movie)
+        {
+            return View(movie);
+        }
+    }
+}
---
+++ Session2/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace Session2
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
